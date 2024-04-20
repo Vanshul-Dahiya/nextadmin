49b5b109cb82eb36145ee9fbfa28cc7d4ad56f62
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2024-04-20 14:02:42 +0530

    Db connected + users fetched

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -2,8 +2,11 @@ import Search from "@/app/ui/dashboard/search/search";
 import styles from "../../ui/dashboard/users/users.module.css";
 import Link from "next/link";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import { fetchUsers } from "@/app/lib/data";
 
-const UsersPage = () => {
+const UsersPage = async () => {
+  const users = await fetchUsers();
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -19,30 +22,32 @@ const UsersPage = () => {
             <td>Email</td>
             <td>Created At</td>
             <td>Role</td>
-            <td>Status</td>
+            {/* <td>Status</td> */}
             <td>Action</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Lisa</td>
-            <td>Lisa@gmail.com</td>
-            <td>13/4/2024</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 20)}</td>
+              <td>{user.isAdmin ? "Admin" : "Member"}</td>
+              {/* <td></td> */}
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
                   </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
---
+++ app/lib/models.js
@@ -0,0 +1,55 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const productSchema = mongoose.Schema(
+  {
+    product: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    costPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    sellPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
+export const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
